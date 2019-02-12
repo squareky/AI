52f234a61d592941c5b9a5011a7d282f9ed4af91
zhong <squarekyzhong@gmail.com>
2019-02-12 17:35:31 -0500

    The project implements two algorithms, naming UCS and A*

---
+++ src/main/java/huristic.java
@@ -0,0 +1,57 @@
+import java.io.IOException;
+
+
+class huristic {
+	private convert vertToString = new convert();
+ 	private String[] verticeStore;
+	private int k = 0;//
+	private int difference = 3;
+	private String[] verticeDetail1= new String[500*3];
+	private int[] verticeDetail2= new int[500*3];
+
+	private int[][] functionCost=new int[500][500];
+	//int[] startVertice = new int[2];
+	//int[] endVertice = new int[2];
+	private int startVerX;
+	private int startVerY;
+	private int endVerX;
+	private int endVerY;
+
+      int[][] computeHust() throws IOException {
+    	 verticeStore = vertToString.convertStringToMatrixVertice();
+    	 for(int i = 0;i < 500;i++) {
+ 			verticeDetail1=verticeStore[i].split(",");
+ 			for(String b: verticeDetail1) {
+ 				verticeDetail2[k] = Integer.parseInt(b);
+ 				k++;
+			}
+    	 }
+    	 
+    	 for(int i = 0;i < 499;i++) {
+    		 for(int j = i+1; j < 500;j++) {
+    			//partition start vertice
+    			 startVerX = verticeDetail2[1+3*i];
+    			 startVerY = verticeDetail2[2+3*i];
+    		    
+    			
+    			//partition end vertice
+    			//verticeDetail2=verticeStore[j].split(",");
+    			 endVerX = verticeDetail2[1+3*j];
+    			 endVerY = verticeDetail2[2+3*j];
+    			if(startVerX==endVerX&&startVerY==endVerY) {
+    			   // functionCost[i][j] = (int)(100*Math.sqrt(Math.abs(endVertice[0]-startVertice[0])*Math.abs(endVertice[0]-startVertice[0])+Math.abs(endVertice[1]-startVertice[1])*Math.abs(endVertice[1]-startVertice[1])));
+    			    functionCost[i][j] =(int)(100*Math.sqrt( Math.abs(endVerX-startVerX)*Math.abs(endVerX-startVerX)+Math.abs(endVerY-startVerY)*Math.abs(endVerY-startVerY)));
+    			    functionCost[j][i] = functionCost[i][j];
+    			}//h(x) = distance
+    			else {
+    				//functionCost[i][j] = (int)(100*Math.sqrt(Math.abs(endVertice[0]-startVertice[0]-1)*Math.abs(endVertice[0]-startVertice[0]-1)+Math.abs(endVertice[1]-startVertice[1]-1)*Math.abs(endVertice[1]-startVertice[1]-1)));//Manhatton block
+    			    functionCost[i][j] =(int) (100*(Math.sqrt((Math.abs(endVerX-startVerX)-1)*(Math.abs(endVerX-startVerX)-1)+(Math.abs(endVerY-startVerY)-1)*(Math.abs(endVerY-startVerY)-1))));
+    			    functionCost[j][i] =functionCost[i][j];
+
+    			    // System.out.println(functionCost[i][j]);
+    			}
+    	 }
+    	 }
+    	 return functionCost;
+     }
+}
